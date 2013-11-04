4c8d86dd6e80c888fccd8b32207325895b0200a6
Ben Straub <bs@github.com>
2013-11-04 06:13:20 -0800

    Add google analytics

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -45,3 +45,14 @@ $(function(){
         setTimeout(function(){ t.addClass('slider'); }, (i+1) * 330);
     });
 });
+
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-35858797-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
