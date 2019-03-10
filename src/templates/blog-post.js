8dca577a850f5008ab2740c9f38b28abf9f7197f
Ben Straub <ben@straub.cc>
2019-03-10 12:27:10 -0700

    Fix spacing on undated pages

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -24,16 +24,18 @@ class BlogPostTemplate extends React.Component {
           description={post.excerpt}
         />
         <h1>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
+        {post.frontmatter.date &&
+          <p
+            style={{
+              ...scale(-1 / 5),
+              display: `block`,
+              marginBottom: rhythm(1),
+              marginTop: rhythm(-1),
+            }}
+          >
+            {post.frontmatter.date}
+          </p>
+        }
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
         {categoryFooters[post.frontmatter.category]}
         <hr
